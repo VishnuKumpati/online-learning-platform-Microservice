33b2694000cb8c9ec8af0c487f0e2c419d90745a
vishnu <vishnuvardhankumpati@gmail.com>
2024-10-19 22:11:03 +0530

    eureka-server

---
+++ eurake-server/src/main/java/com/learn/eurake_server/EurakeServerApplication.java
@@ -0,0 +1,13 @@
+package com.learn.eurake_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurakeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurakeServerApplication.class, args);
+	}
+
+}
